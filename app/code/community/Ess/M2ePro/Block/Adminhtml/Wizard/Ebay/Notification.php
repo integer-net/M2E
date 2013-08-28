de425b60a4edd3f2755a0743cc4c1254493866ee
Andreas von Studnitz <avs@integer-net.de>
2013-08-28 15:19:32 +0200

    Removed old files

--- app/code/community/Ess/M2ePro/Block/Adminhtml/Wizard/Ebay/Notification.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
-* @copyright  Copyright (c) 2011 by  ESS-UA.
-*/
-
-class Ess_M2ePro_Block_Adminhtml_Wizard_Ebay_Notification extends Ess_M2ePro_Block_Adminhtml_Wizard_Notification
-{
-}
