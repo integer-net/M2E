28ab01ea10d657d7940421bd976e295a4393cd9f
Alexander Turiak <alex@hexbrain.com>
2015-05-20 09:28:30 +0200

    Use Mage_Core_Controller_Front_Action after SUPEE-5994

--- src/app/code/community/Ess/M2ePro/controllers/CronController.php
+++ src/app/code/community/Ess/M2ePro/controllers/CronController.php
@@ -4,7 +4,7 @@
  * @copyright  Copyright (c) 2013 by  ESS-UA.
  */
 
-class Ess_M2ePro_CronController extends Mage_Core_Controller_Varien_Action
+class Ess_M2ePro_CronController extends Mage_Core_Controller_Front_Action
 {
     //#############################################
 
